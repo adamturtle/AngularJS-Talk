e4913ea07095e5514cc1462e45ce64d00badecd6
Adam Turtle <adamt@addsaltandpepper.com>
2014-10-23 17:49:31 -0400

    Update

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-var express = require("express");
+var express = require('express')
 var app = express();
-app.use(express.logger());
-app.use("/", express.static(__dirname));
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/app'))
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
